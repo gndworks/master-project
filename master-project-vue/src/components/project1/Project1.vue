6b5c8d615be65a6993606053b476f76e8aefbdc4
Guan <gndworks@outlook.com>
2025-06-16 21:55:06 +1000

    Add rentInfo

--- master-project-vue/src/components/project1/Project1.vue
+++ master-project-vue/src/components/project1/Project1.vue
@@ -2,13 +2,14 @@
   <v-grid>
     <v-row><basic-info/></v-row>
     <v-row>
-      <v-col></v-col>
-      <v-col></v-col>
+      <v-col><rent-info/></v-col>
+      <v-col><rent-info/></v-col>
     </v-row>
   </v-grid>
 </template>
 
 <script setup lang="ts">
 import BasicInfo from './BasicInfo.vue';
+import RentInfo from './RentInfo.vue';
 
 </script>
---
+++ master-project-vue/src/components/project1/RentInfo.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-form>
+    <v-text-field
+      label="Yearly Income"
+      v-model="weeklyRentalPrice"
+      type="number"
+      prepend-icon="mdi-currency-usd"
+      required
+    ></v-text-field>
+    <v-text-field
+      label="Current Savings"
+      v-model="yearlyRentIncrease"
+      type="number"
+      prepend-icon="mdi-bank"
+      required
+    ></v-text-field>
+  </v-form>
+</template>
+
+<script setup lang="ts">
+import { useRentalInformation } from './composables/rentalInformation';
+
+const { weeklyRentalPrice, yearlyRentIncrease } = useRentalInformation();
+</script>
